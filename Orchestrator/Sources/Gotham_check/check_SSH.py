b724049cf26a7c29512e92ad8f25af86de2cb5fe
Bastien <bastien1453@gmail.com>
2021-01-21 15:59:50 +0000

    fix{Gotham_check/check_SSH.py}: Ajout des commentaires

--- Orchestrator/Sources/Gotham_check/check_SSH.py
+++ Orchestrator/Sources/Gotham_check/check_SSH.py
@@ -3,11 +3,18 @@ from io import StringIO
 from Gotham_SSH_SCP import execute_commands
 
 def main(ip, ssh_port, used_ssh_key):
+    # Check if the SSH server is operational
+    #
+    # ip (string) : ip of the remote server
+    # ssh_port (int) : port on which the SSH service listen
+    # used_ssh_key (file-like object) : Key used to auth to the server
+    #
+    # Return True if succeed, False in the other case
     command_exec_check = ["echo 'alive' > /tmp/gotham_status && rm -rf /tmp/gotham_status"]
     # Try to execute the commands to the remote server
     try:
-        execute_commands(ip, "192.168.1.171", ssh_port, used_ssh_key, command_exec_check)
-    except:
+        execute_commands(ip, ssh_port, used_ssh_key, command_exec_check)
+    except Exception as e:
         # If it's not possible, return False
         return False
     # If we were able to execute the commands, return True
