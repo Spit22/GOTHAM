3d07075ee807dc2bb3de88cef2cbaa01cb493af3
Bastien <bastien1453@gmail.com>
2021-01-22 13:10:09 +0000

    feat{api.py}: ajout de store hp

--- Orchestrator/Sources/api.py
+++ Orchestrator/Sources/api.py
@@ -3,6 +3,7 @@ import flask
 import json
 import uuid
 import base64
+import os
 from flask import request, jsonify
 from io import StringIO # a suppr
 
@@ -10,7 +11,7 @@ from io import StringIO # a suppr
 import add_server  
 import Gotham_link_BDD
 import Gotham_check
-
+import add_hp
 
 app = flask.Flask(__name__)
 
@@ -18,6 +19,59 @@ app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 version = "0.0"
 db_settings = {"username":"root", "password":"password", "hostname":"localhost", "port":"3306", "database":"GOTHAM"}
+dc_ports_list = range(1024,2048)
+dockerfile_storage = "/data/"
+dc_ip = "172.16.2.250"
+dc_ssh_port = "22"
+dc_ssh_key = StringIO("""-----BEGIN OPENSSH PRIVATE KEY-----
+b3BlbnNzaC1rZXktdjEAAAAABG5vbmUAAAAEbm9uZQAAAAAAAAABAAACFwAAAAdzc2gtcn
+NhAAAAAwEAAQAAAgEAsWirX8jlb3WJY5nEQtRV4KOZSUMoERJw1lAxaf/WkXKjV3u7rCd4
+X80VT83Pi3YXZTbgehaPC38tFXPFyHumXFU0YSQQbzEyqnWAv4ZjiKPUwIHxBAWmPsAUVd
+/AsgkzIOOpo8vf+VtVxC8inXdSwUeKJIltl5NXwb9Pe5SzpNih0B2h+s9xYpSkislblXhZ
+FvoA58osab51ucCVEildwnb1HR4ddOT5JRBMw+fjnlAzuYCAG+Y4qGiXu+AOP92hqu9c3M
+fzJE1S7uDjojdeojC1X22qyRLu/sZgsOF450xJynF0pgydjxhwHPGgTgfv3UKWBH9hf8Fb
+eiS0snfSJXjW+unWEMNxmjIBP+HeuLaoAMtDk2BDqbARsAlhe5GDCjYDPf/TrRnobG1pBi
+FK1k9QW5pdYuUwhO9FpedqCYpDv/9F6os6UKepZM8SKI0/8W5uyIk2Q+DcPMPOtWMG41yv
+qdVk81fiurklmoOtc8Y7igWxFnhyUe9kTta3CbgQBVqYEgUKzU0YxrK1CrwRdr9FeHEAOT
+rpKM/CLfmofKPwGiLg+VXYdvvWdvXjqMQxAIfffrFSpPuKM8EIQqS40KQvhOZUhM3t6hpn
+GH5CMwaLglZq2ZijrKDe4gN3UmQoDfcuFGN2S127B+3V6pFopJ8lkfccWM04cScEWTyXLf
+0AAAdI9cSOMPXEjjAAAAAHc3NoLXJzYQAAAgEAsWirX8jlb3WJY5nEQtRV4KOZSUMoERJw
+1lAxaf/WkXKjV3u7rCd4X80VT83Pi3YXZTbgehaPC38tFXPFyHumXFU0YSQQbzEyqnWAv4
+ZjiKPUwIHxBAWmPsAUVd/AsgkzIOOpo8vf+VtVxC8inXdSwUeKJIltl5NXwb9Pe5SzpNih
+0B2h+s9xYpSkislblXhZFvoA58osab51ucCVEildwnb1HR4ddOT5JRBMw+fjnlAzuYCAG+
+Y4qGiXu+AOP92hqu9c3MfzJE1S7uDjojdeojC1X22qyRLu/sZgsOF450xJynF0pgydjxhw
+HPGgTgfv3UKWBH9hf8FbeiS0snfSJXjW+unWEMNxmjIBP+HeuLaoAMtDk2BDqbARsAlhe5
+GDCjYDPf/TrRnobG1pBiFK1k9QW5pdYuUwhO9FpedqCYpDv/9F6os6UKepZM8SKI0/8W5u
+yIk2Q+DcPMPOtWMG41yvqdVk81fiurklmoOtc8Y7igWxFnhyUe9kTta3CbgQBVqYEgUKzU
+0YxrK1CrwRdr9FeHEAOTrpKM/CLfmofKPwGiLg+VXYdvvWdvXjqMQxAIfffrFSpPuKM8EI
+QqS40KQvhOZUhM3t6hpnGH5CMwaLglZq2ZijrKDe4gN3UmQoDfcuFGN2S127B+3V6pFopJ
+8lkfccWM04cScEWTyXLf0AAAADAQABAAACAF1PdkPNAJAdPAP9DnMwB3M92RPllJ9WGa8/
+Qp5EB/E8YJlU5SLpJ7ELxxfQYkcx96Auuua8EsSzQV01JWQLVTbfQcuOEm7Ja6KoZu1Vm+
+h0cyRFtCSva/85O/jm1Q1PNWspE0KpqsYWugeT2xsDda1fGVOaTOAaiV/IZd/UGKCtqH4z
+98xZa4O0Ns8glWEKiaFCIVPIzI9Zs8XdewqwYzYzJz0HZ+rhvAE4j4fC4b+U43/ADKGf91
+DsolIYyKXixnOHrkoZiNMhMGugMVpS0R1xjRQCNH/Jy5aJdLXUAal2QKGQBxCHNh/bdKxk
+kjI4jnrL2OfIr9loM9sMj1YHG6nlCoRy+Mxnzm7tZSqCUEbVo6naz42DpxtOcAVGKQVfge
+ifJ9ZUhUN/UVLCPRMC6Pmcm3Ia0RTydmVfcPR8JnuEAS8sORXVZSVYH+jXGkYpkMMPIwlG
+s+95qRWErpYouVvyHfB3cEFFPjTDTeyumGxe/4gX57znPd14ksIDZH28CcfZvP4zrB+HOO
+ibvNvCeGmVhboLTmkE/igdPIZ+9S89bnGI10VX4BGk2NIXF4X00PxKCR95CKyixdIn1hs/
+YyJ6HOLamO3Ty613WC13yO5HZJLam9riPF4ym7pt92JEl+MXAoCFhh9SlvxIeFfQ0MnfZi
+QKMAJ40JmyP+r66GPBAAABAFfd6W/lyag0xhdZnA9WLLXHgYpDRUmvZS8tQx+X1QZn39Os
+gGt0K24T6+VYaTZVzL1vS1CxLzLyFw3YFKGedYlsjUsC2CYCf+Ck31FOP8EBvHVVH6PBhp
+aYX1HQPCdAzUxyqrFNl4htDYgyggPJn/yHeOu+IZWpXTBRFKULLJ2SedJehRAWNR9Z4rrP
+pTGHu6XwzedFtc07F8Ip3UEuVVd2JC7LXxPZXtx02W7Et48MEOwW+GVzYuZn8JCpJ/TRw+
+dTCFwJevAyunFNpxr25Vf6hf0Nx/qt8s/4/v5pbRRuUWpFidlkLQhYiYgayLePCRPaAVkT
+0fpVWLQ8jFpXBewAAAEBAOYqESUilSiyhXtspBwrNRWJAV0L2K6F3x1Ta1x6hAIZJ3s6p5
+MkhQoWaXVmuH8uxytKLzIPoQSPEgufLvV1I++e00hY9aUV/872i+D/0fQwPn4+Dcwlmn+D
+3y7OR4NLC7/bmc5B77WECER4a1umdzUc6mv7l+7ZKpI14udmBU4I+qTTs0F1A5DYzJ5Pve
+Q3Qndnqf5Ljy2QyMo/Eldkq9yHJu9daAQxrdjanrwRJARp5DrXgqpIq5PFtJso+5dxvHzw
+sX/w+Tx1V6V9ZjXvdtLMvQZHTojEKXdPgBsTw9hRET+4zKHLkJR5cmZXXxqFs3jc+ISgts
+OY3DYYSFevLAkAAAEBAMVSoyxp7tVaIZAQj8ZGUU5tj8nDocaqU3s0l+Xv/I/ZhgkxFfQ5
+seXeNKv9c6S5UvcCmnR9WYUq7xhsGxU5+6Se3txTJfTeh1HVnLxwFYpwSWANIyznJRF9Fu
+ks9/UJiTCmtebHzveta6rqlrI7Ga8cC7GJBFScMYVBHikqxqY/RyrWCAta01m1ZR+/a4kV
+Pxg06Ro4Vozajsjrg+EoL8MxpK0VdRLt95s/w0DdotMvLkfpPcwwNae8+VILZjQSd/lxJG
+otmBlmJPbHljWt2JWympm3wVI24J0Ag4nRbSff5tDgH/5WsnaBTJKsohAUuc3Ut3LmAkCe
+Lojn7XZi11UAAAARcm9vdEBkZWJpYW4xMC1ubXMBAg==
+-----END OPENSSH PRIVATE KEY-----""")
 ###################################################################################################
 
 @app.route('/', methods=['GET'])
@@ -25,7 +79,7 @@ def index():
         return 'You\'re on the GOTHAM\'s API'
 
 @app.route('/add/honeypot', methods=['POST'])
-def add_hp():
+def add_honeypot():
         # Creates a honeypot object
         # 
         # name (string) : nom du honeypot
@@ -33,20 +87,64 @@ def add_hp():
         # tags (list) : liste des tags du honeypot
         # logs (string) : path des logs à monitorer
         # parser (string) : règle de parsing des logs monitorés
+        # dockerfile (string) : dockerfile to generate the honeypot on datacenter, base64 encoded
+        # service_port (int) : port on which the honeypot will lcoally listen
 
         # Get POST data on JSON format
-        data = request.json
-
-        # Make sure all data are in JSON
-        data = json.loads(data)
+        data = request.get_json()
 
         # Get all function's parameters
         name = data["name"]
         descr = data["descr"]
         tags = data["tags"]
-        logs = data["parser"]
+        logs = data["logs"]
+        parser = data["parser"]
+        dockerfile = data["dockerfile"]
+        dockerfile = base64.b64decode(dockerfile)
+        dockerfile = dockerfile.decode('ascii')
+        dockerfile = str(dockerfile)
+        port = data["port"]
+        
 
-        return "NOT IMPLEMENTED"
+        # First find an available port to map on datacenter
+        #for mapped_port in dc_ports_list:
+        #    if Gotham_check.is_dc_port_available(mapped_port):
+        #        available_port = True
+        #        break
+        #    available_port = False
+
+        # Check if an available port was found
+        #if not available_port:
+        #    return "Datacenter : no port available for mapping"
+        # TEMP
+        mapped_port = "2200"
+        # Generate honeypot's id
+        id = 'hp-'+str(uuid.uuid4().hex)
+
+        # Write the dockerfile in the local storage
+        dockerfile_path = str(dockerfile_storage)+str(id)+"/"
+        if not os.path.isdir(dockerfile_path):
+            os.mkdir(dockerfile_path)
+            dockerfile_file = open(str(dockerfile_path)+"Dockerfile", 'a')
+            dockerfile_file.write(dockerfile)
+            dockerfile_file.close()
+        else:
+            return "Can't store the dockerfile locally : path already exists"
+
+        # Generate docker-compose.yml from information
+        add_hp.generate_dockercompose(id, dockerfile_path, logs, port, mapped_port)
+        
+        # Deploy the hp's container on datacenter
+        dc_connect = add_hp.deploy_container(dc_ip, dc_ssh_port, dc_ssh_key, dockerfile_path)
+        if not dc_connect:
+            return "An error occured in the ssh connection"
+
+        # Store new hp and tags in the database
+        sql_data = {'id':str(id),'name':str(name),'descr':str(descr),'tag':str(tags),'port':str(port),'parser':str(parser),'logs':str(logs),'source':str(dockerfile_path),'state':'INACTIVE','id_container':'NULL'}
+        Gotham_link_BDD.add_honeypot_DB(db_settings, sql_data)
+
+        # If all operations succeed
+        return "OK : "+str(id)
 
 @app.route('/add/server', methods=['POST'])
 def add_srv():
@@ -89,7 +187,7 @@ def add_srv():
         if not connected:
             return "Provided ssh_key or ssh_port is wrong"
 
-	# If all checks are ok, we can generate a an id for the new server
+	# If all checks are ok, we can generate an id for the new server
         id = 'sv-'+str(uuid.uuid4().hex)
 
 	# Deploy the reverse-proxy service on the new server
@@ -98,7 +196,7 @@ def add_srv():
         except Exception as e:
             return "Something went wrong while deploying Reverse-Proxy"
 
-	# Store new server and tags on the internal database
+	# Store new server and tags in the internal database
         try:
             sql_data = {'id':str(id),'name':str(name),'descr':str(descr),'tag':str(tags),'ip':str(ip),'ssh_key':str(ssh_key),'ssh_port':str(ssh_port),'state':'INACTIVE'}
             Gotham_link_BDD.add_server_DB(db_settings, sql_data)
@@ -109,7 +207,7 @@ def add_srv():
         return "OK : "+str(id)
 
 @app.route('/add/link', methods=['POST'])
-def add_link():
+def add_lk():
         # Creates a link object
         # 
         # tag_srv (list) : tag du/des serveurs ciblés
