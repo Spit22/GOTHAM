d12f47be54df00f4c9d2f8ea4b45f6d647342564
Spit22 <spit22@protonmail.com>
2021-01-22 21:36:48 +0100

    {add Orchestrator/sources/Gotham_check/check_used_port} : add a function to retrieve a list of used port

--- Orchestrator/Sources/Gotham_check/__init__.py
+++ Orchestrator/Sources/Gotham_check/__init__.py
@@ -1,6 +1,9 @@
 from . import check_SSH
 from . import check_PING
 from . import check_DOUBLON
+from . import check_USED_PORT
+import mariadb
+import sys
 
 def check_ssh(ip, ssh_port, ssh_key):
     '''
@@ -35,4 +38,26 @@ def check_doublon_tag(DB_settings, tag):
         tag (string) : name of the tag you want to check
     '''
     return check_DOUBLON.tag(DB_settings, tag)
+
+def check_used_port(DB_settings):
+    '''
+    Retrieve a list of all used port by honeypots from the internal database
+
+    ARGUMENTS:
+        DB_settings (dict) : all the settings to connect to the internal database
+    '''
+    try:
+        DB_connection = mariadb.connect(
+            user=DB_settings["username"],
+            password=DB_settings["password"],
+            host=DB_settings["hostname"],
+            port=int(DB_settings["port"]),
+            database=DB_settings["database"]
+        )
+    except mariadb.Error as e:
+        print(f"Error connecting to MariaDB server: {e}")
+        sys.exit(1)
+    result = check_USED_PORT.get_used_port(DB_connection)
+    DB_connection.close()
+    return result
     
---
+++ Orchestrator/Sources/Gotham_check/check_USED_PORT.py
@@ -0,0 +1,16 @@
+import sys
+
+
+def get_used_port(DB_connection):
+    # Get MariaDB cursor
+    cur = DB_connection.cursor()
+    # Execute SQL request
+    cur.execute("SELECT DISTINCT port FROM Honeypot")
+    # Convert answer to JSON
+    rv = cur.fetchall()
+    res=[]
+    for result in rv:
+       res.append(result[0])
+    # Before the return, close the connection
+    return res
+
