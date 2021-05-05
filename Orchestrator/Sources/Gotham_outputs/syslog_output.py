ddaebab9cd8e02bfaf886fe422c3870d72304287
spit22 <spit22@protonmail.com>
2021-05-05 04:53:20 +0200

    fix{Orchestrator/Sources/Gotham_outputs} : fix issue

--- Orchestrator/Sources/Gotham_outputs/syslog_output.py
+++ Orchestrator/Sources/Gotham_outputs/syslog_output.py
@@ -15,9 +15,11 @@ def main(hostname, syslog_port, protocol):
     # port (string) : port of syslog server
 
     # Get POST data on JSON format
+    digit_list = hostname.split('.')
+    name_of_host = digit_list[0] + "-" + digit_list[1] + "-" + digit_list[2] + "-" + digit_list[3]
     try:
         # Create the configuration file
-        rsyslog_conf_file = open("/etc/rsyslog.d/10-syslog-" + str(hostname) + "-" + str(syslog_port) + ".conf", "a")
+        rsyslog_conf_file = open("/etc/rsyslog.d/10-syslog-" + str(name_of_host) + "_" + str(syslog_port) + ".conf", "a")
         # Monitor the log file of the honeypot
         rsyslog_conf_file.write('if $msg contains "hp-" then {\n')
         # Apply parsing rules
