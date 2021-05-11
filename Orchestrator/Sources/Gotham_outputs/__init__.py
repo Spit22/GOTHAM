e2262873ffb459ecb2702ca49002c5160acaa61c
spit22 <spit22@protonmail.com>
2021-05-11 10:55:46 +0200

    edit{Orchestrator/Sources/Gotham_output} : improve output management

--- Orchestrator/Sources/Gotham_outputs/__init__.py
+++ Orchestrator/Sources/Gotham_outputs/__init__.py
@@ -2,19 +2,73 @@
 from . import syslog_output
 #==========================#
 
+import os
+import subprocess
+import configparser
 
-def syslog(hostname, syslog_port, protocol):
+#===Logging components===#
+import os
+import logging
+GOTHAM_HOME = os.environ.get('GOTHAM_HOME')
+logging.basicConfig(filename=GOTHAM_HOME + 'Orchestrator/Logs/gotham.log',
+                    level=logging.DEBUG, format='%(asctime)s -- %(name)s -- %(levelname)s -- %(message)s')
+#=======================#
+
+def syslog():
     '''
-    Create a syslog output
-
-    ARGUMENTS:
-        hostname (string) : hostname of the remote syslog server
-        port (string) : syslog port of the remote syslog server
+    Manage syslog outputs
     '''
-    try:
-        syslog_output.main(hostname, syslog_port, protocol)
-    except Exception as e:
-        error = "Fail to create syslog output"
-        raise ValueError(error)
-
+    # Retrieve syslog output configuration
+    config = configparser.ConfigParser()
+    config.read(GOTHAM_HOME + 'Orchestrator/Config/config.ini')
+    if not(config.has_section('syslog')):
+        return False
     
+    # List existing syslog outputs
+    included_extensions = ['conf']
+    included_prefix = ['10-syslog']
+    existing_configuration = [f for f in os.listdir("/etc/rsyslog.d/") if any(f.startswith(pref) for pref in included_prefix) and any(f.endswith(ext) for ext in included_extensions)]
+
+    # List required syslog outputs
+    configuration_required = []
+    for key, value in config.items("syslog"):
+        value = value.split(',')
+        hostname = value[0]
+        syslog_port = value[1]
+        protocol = value[2]
+
+        digit_list = hostname.split('.')
+        name_of_host = digit_list[0] + "-" + digit_list[1] + "-" + digit_list[2] + "-" + digit_list[3]
+        configuration_required.append("10-syslog_" + str(protocol) + "_" + str(name_of_host) + "_" + str(syslog_port) + ".conf")
+
+    # Find new outputs and outputs to delete
+    outputs_to_create = list(set(configuration_required) - set(existing_configuration))
+    outputs_to_delete = list(set(existing_configuration) - set(configuration_required))
+    print(outputs_to_create)
+    print(outputs_to_delete)
+
+    # Create required outputs
+    for new_syslog_output in outputs_to_create:
+        try:
+            syslog_output.create(new_syslog_output, hostname, syslog_port, protocol)
+            logging.debug(f"Syslog output created with following parameters : {protocol}@{hostname}:{syslog_port}")
+        except Exception as e:
+            error = "Fail to create syslog output"
+            raise ValueError(error)
+    
+    # Delete obsolete outputs
+    for obsolete_syslog_output in outputs_to_delete:
+        try:
+            syslog_output.delete(obsolete_syslog_output)
+            logging.debug(f"Syslog output created with following parameters : {protocol}@{hostname}:{syslog_port}")
+        except Exception as e:
+            error = "Fail to create syslog output"
+            raise ValueError(error)
+    # Restart rsyslog
+    try:
+        subprocess.run(["systemctl", "restart", "rsyslog"])
+    except Exception as e:
+        error = "Fail to deploy syslog output : " + str(e)
+        logging.error(error)
+        raise ValueError(error)
+    return True
--- Orchestrator/Sources/Gotham_outputs/syslog_output.py
+++ Orchestrator/Sources/Gotham_outputs/syslog_output.py
@@ -8,22 +8,20 @@ logging.basicConfig(filename=GOTHAM_HOME + 'Orchestrator/Logs/gotham.log',
                     level=logging.DEBUG, format='%(asctime)s -- %(name)s -- %(levelname)s -- %(message)s')
 #=======================#
 
-def main(hostname, syslog_port, protocol):
+def create(new_syslog_output, hostname, syslog_port, protocol):
     # Create a syslog output
     #
     # hostname (string) : hostname of syslog server
     # port (string) : port of syslog server
 
     # Get POST data on JSON format
-    digit_list = hostname.split('.')
-    name_of_host = digit_list[0] + "-" + digit_list[1] + "-" + digit_list[2] + "-" + digit_list[3]
     try:
         # Create the configuration file
-        rsyslog_conf_file = open("/etc/rsyslog.d/10-syslog-" + str(name_of_host) + "_" + str(syslog_port) + ".conf", "a")
+        rsyslog_conf_file = open("/etc/rsyslog.d/" + str(new_syslog_output), "a")
         # Monitor the log file of the honeypot
         rsyslog_conf_file.write('if $msg contains "hp-" then {\n')
         # Apply parsing rules
-        rsyslog_conf_file.write('action(Type="omfwd" Target="' + str(hostname) + '" Port="1514" Protocol="' + str(protocol) + '" Template="RawFormat")}\n')
+        rsyslog_conf_file.write('action(Type="omfwd" Target="' + str(hostname) + '" Port="' + str(syslog_port) + '" Protocol="' + str(protocol) + '" Template="RawFormat")}\n')
     except Exception as e:
         error = "Fail to generate syslog output configuration file"
         logging.error(error)
@@ -34,3 +32,7 @@ def main(hostname, syslog_port, protocol):
         error = "Fail to deploy syslog output configuration : " + str(e)
         logging.error(error)
         raise ValueError(error)
+
+def delete(obsolete_syslog_output):
+    # Delete a syslog output
+    os.remove("/etc/rsyslog.d/" + str(obsolete_syslog_output))
