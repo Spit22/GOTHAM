778837e02877891d744097eeb4949a97d2e59ff4
spit22 <spit22@protonmail.com>
2021-05-12 15:08:27 +0200

    fix{Orchestrator/Sources/add_hp} : fix issue

--- Orchestrator/Sources/add_hp.py
+++ Orchestrator/Sources/add_hp.py
@@ -201,8 +201,8 @@ def deploy_rsyslog_conf(datacenter_settings, orchestrateur_settings, id_hp, rule
         raise ValueError(error)
     
     # Check if required directories on datacenter exists
-    rsyslog_conf_datacenter_remote_path_exists = execute_command_with_return(orchestrateur_settings["hostname"], datacenter_settings["ssh_port"], datacenter_settings["ssh_key"], f"[[ -d {rsyslog_conf_datacenter_remote_path} ]] && echo 'OK'")
-    remote_rulebase_path_exists = execute_command_with_return(orchestrateur_settings["hostname"], datacenter_settings["ssh_port"], datacenter_settings["ssh_key"], f"[[ -d {remote_rulebase_path} ]] && echo 'OK'")
+    rsyslog_conf_datacenter_remote_path_exists = execute_command_with_return(datacenter_settings["hostname"], datacenter_settings["ssh_port"], datacenter_settings["ssh_key"], f"[[ -d {rsyslog_conf_datacenter_remote_path} ]] && echo 'OK'")
+    remote_rulebase_path_exists = execute_command_with_return(datacenter_settings["hostname"], datacenter_settings["ssh_port"], datacenter_settings["ssh_key"], f"[[ -d {remote_rulebase_path} ]] && echo 'OK'")
     if not(rsyslog_conf_datacenter_remote_path_exists == 'OK' and remote_rulebase_path_exists == 'OK'):
         error = "At least one directory on datacenter is missing"
         logging.error(error)
