f6d7997a339591d8210e36d42f57527a96adf03a
root <root@debian10-nms>
2021-05-20 21:47:35 +0000

    feat{install_orchestrator.sh}: restructuration du code

--- Install/install_orchestrator.sh
+++ Install/install_orchestrator.sh
@@ -1,41 +1,44 @@
 #!/bin/bash
+#
+# Authors : GOTHAM Team
+# Descr : this script permits to prepare te orchestrator system and install GOTHAM
+#
+# Last update : 20/05/2021
+
+#set -e pipefail
+
+### DEFINE GLOBAL VARIABLES ###
+GOTHAM_HOME=/opt/GOTHAM/
+GOTHAM_GIT="https://github.com/Spit22/GOTHAM"
+EXEC_BRANCH="dev_v1_1"
+
+### DEFINE BINARY PATHS ###
+USERADD=$(which useradd)
+GIT=$(which git)
+CHOWN=$(which chown)
+CHMOD=$(which chmod)
+APT=$(which apt)
+PIP3=$(which pip3)
+
+### CHECK IF ROOT ###
+if [ "$EUID" -ne 0 ]
+  then echo "[-] Please run this script as root"
+  exit
+fi
+
+### PREPARE GOTHAM SYSTEM ###
+echo "=== Preparing the gotham system... ==="
 
 # Create GOTHAM user as an alias for the root account
-/usr/sbin/useradd -o -u 0 -g 0 -N -d /root/ -M gotham
+$USERADD -o -u 0 -g 0 -N -d /root/ -M gotham > /dev/null 2>&1
+echo "[+] User gotham created"
 
-# Update repository
-apt update
+# Generate the GOTHAM profile
+echo -e "#GOTHAM global variables\nexport GOTHAM_HOME=$GOTHAM_HOME" > /etc/profile.d/gotham.sh
+$CHMOD +x /etc/profile.d/gotham.sh
+echo "[+] Gotham profile generated under /etc/profile.d/gotham.sh"
 
-# Install needed packages
-apt install -y python3 python3-pip git mariadb-server
-
-# Install libs
-apt install -y libmariadb-dev
-
-# Going to /opt to install GOTHAM
-cd /opt
-
-# Configure $GOTHAM_HOME var
-echo -e "\nexport GOTHAM_HOME=/opt/GOTHAM/" >> /etc/profile
-GOTHAM_HOME=/opt/GOTHAM/
-
-# Clone repository on this directory
-git clone https://github.com/Spit22/GOTHAM
-chown -R gotham:root $GOTHAM_HOME
-
-
-# Going on the repository folder
-cd $GOTHAM_HOME/Orchestrator/Sources
-
-# Choose the git branch
-git checkout dev_V1_1
-
-
-# Install all python libs
-pip3 install -r requirements.txt
-
-# Create /data to store GTOAHM's data
-mkdir -p $GOTHAM_HOME/Orchestrator/Logs
+# Create some paths used by GOTHAM
 mkdir -p /data/template
 mkdir -p /data/rsyslog/datacenter-configuration
 mkdir -p /data/rsyslog/servers-configuration
@@ -43,6 +46,26 @@ mkdir -p /data/honeypot-log
 mkdir -p /data/link-log
 mkdir -p /data/rsyslog/rulebase
 mkdir -p /etc/rsyslog.d
+echo "[+] All paths created"
+
+
+### INSTALL DEPENDANCIES ###
+echo "=== Installing dependancies... ==="
+
+# Update repository
+$APT update > /dev/null 2>&1
+echo "[+] APT updated"
+
+# Install needed packages
+$APT install -y python3 python3-pip git mariadb-server git > /dev/null 2>&1
+echo "[+] Some packages correctly installed"
+
+# Install libs
+$APT install -y libmariadb-dev > /dev/null 2>&1
+echo "[+] Some libraries correctly installed"
+
+### INSTALL RSYSLOG ###
+echo "=== Configuring Rsyslog... ==="
 
 # Pre-configure rsyslog
 touch /etc/rsyslog.d/00-syslog_server.conf
@@ -53,14 +76,33 @@ input(type="imtcp" port="1514")
 ''' > /etc/rsyslog.d/00-syslog_server.conf
 
 # Restart rsyslog
-systemctl restart rsyslog
+systemctl restart rsyslog > /dev/null 2>&1
+echo "[+] Rsyslog configured under /etc/rsyslog.d/"
 
-# Copy configuration template to /etc/gotham
-#mkdir /etc/gotham/
-#cp $GOTHAM_HOME/Orchestrator/Config/config.ini
+
+### INSTALL GOTHAM ###
+echo "=== Installing GOTHAM... ==="
+
+# Clone repository on this directory
+$GIT clone $GOTHAM_GIT $GOTHAM_HOME > /dev/null 2>&1
+$CHOWN -R gotham:root $GOTHAM_HOME
+echo "[+] GOTHAM cloned under $GOTHAM_HOME"
+
+# Going on the repository folder
+cd $GOTHAM_HOME
+
+# Choose the git branch
+$GIT checkout $EXEC_BRANCH > /dev/null 2>&1
+echo "[+] Switched to $EXEC_BRANCH"
+
+# Install all python libs
+$PIP3 install -r $GOTHAM_HOME/Orchestrator/Sources/requirements.txt > /dev/null 2>&1
+echo "[+] Pip dependancies installed"
+
+# Create the log folder
+mkdir -p $GOTHAM_HOME/Orchestrator/Logs
 
 # Configure database
-mysql -u root -e "CREATE DATABASE GOTHAM;"
-mysql -u root GOTHAM < $GOTHAM_HOME/Orchestrator/Internal_Database/gotham.sql
-
-source /etc/profile
+mysql -u root -e "CREATE DATABASE GOTHAM;" > /dev/null 2>&1
+mysql -u root GOTHAM < $GOTHAM_HOME/Orchestrator/Internal_Database/gotham.sql > /dev/null 2>&1
+echo "[+] MySQL db successfully imported"
