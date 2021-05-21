e18637cdcd2c8e81a36112236ad7444814ac1a1b
Baxytra <baxytra@protonmail.com>
2021-05-21 10:58:30 +0200

    fix{Install/install_server.sh}: restructuration du code

--- Install/install_server.sh
+++ Install/install_server.sh
@@ -1,10 +1,46 @@
 #!/bin/bash
+#
+# Authors : GOTHAM Team
+# Descr : this script permits to prepare the server environment
+#
+# Last update : 21/05/2021
 
-# RSYSLOG PART
-# Create the folder used by GOTHAM
+set -e pipefail
+
+### DEFINE GLOBAL VARIABLES ###
+
+### DEFINE BINARY PATHS ###
+USERADD=$(which useradd)
+USERMOD=$(which usermod)
+CHMOD=$(which chmod)
+KEYGEN=$(which ssh-keygen)
+ENCODE=$(which base64)
+
+### CHECK IF ROOT ###
+if [ "$EUID" -ne 0 ]
+  then echo "[-] Please run this script as root"
+  exit
+fi
+
+### PREPARE GOTHAM SYSTEM ###
+echo "=== Preparing the gotham system... ==="
+
+# Create GOTHAM user as an alias for the root account
+$USERADD -o -u 0 -g 0 -N -d /root/ -M gotham > /dev/null 2>&1
+
+# Configure password
+mdp=$(tr -cd '[:alnum:]' < /dev/urandom | fold -w12 | head -n1)
+$USERMOD -p "$mdp" gotham
+echo "[+] User gotham created with password : $mdp"
+
+# Create the folder used by GOTHAM servers
 mkdir -p /data/tmp
 mkdir -p /data/rsyslog/rulebase
 mkdir -p /etc/rsyslog.d
+echo "[+] All paths created"
+
+### INSTALL RSYSLOG ###
+echo "=== Configuring Rsyslog... ==="
 
 # Pre-configure rsyslog
 touch /etc/rsyslog.d/00-JSON_template.conf
@@ -19,12 +55,17 @@ template(name="all-json-template" type="list"){
 
 # Restart rsyslog
 systemctl restart rsyslog
+echo "[+] Rsyslog configured"
+
+### INSTALL AND CONFIGURE SSH SERVER ###
+echo "=== Installing and configuring SSH... ==="
+# Update repository
+$APT update > /dev/null 2>&1
+echo "[+] APT updated"
 
 # Installation of openssh and base64
-apt install -y openssh-server
-
-# Create GOTHAM user as an alias for the root account
-/usr/sbin/useradd -o -u 0 -g 0 -N -d /root/ -M gotham
+$APT install -y openssh-server > /dev/null 2>&1
+echo "[+] SSH Server installed"
 
 # Harden SSH configuration
 echo """
@@ -39,19 +80,20 @@ PubkeyAuthentication yes
 
 # Restart openssh
 service ssh restart
+echo "[+] SSH Server configured"
+
+### GENERATE SSH KEY ###
+echo "=== Generating SSH Key... ==="
 
 # Generate a rsa 4096 key pair without passphrase
-ssh-keygen -b 4096 -t rsa -f /root/gotham_key -N ""
+$KEYGEN -b 4096 -t rsa -f /root/gotham_key -N ""
 
 # Copy the public key to authorized keys of root users
 mkdir -p /root/.ssh
 pubkey=$(cat /root/gotham_key.pub)
 echo $pubkey >> /root/.ssh/authorized_keys
-chmod -R 600 /root/.ssh
-
-# Désactiver le mot de passe
-/usr/sbin/usermod -p "default-gotham's-password" gotham
+$CHMOD -R 600 /root/.ssh
 
 # Show base64 encoded private key user has to send to api
-echo "\n\nPrivate key : \n"
-/usr/bin/base64 /root/gotham_key | tr -d "\n"
+echo "\n\n[+]Private key generated : \n"
+$ENCODE /root/gotham_key | tr -d "\n"
