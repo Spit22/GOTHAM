fe660ae407cf372dcdf628b191b75da703b945f4
spit22 <spit22@protonmail.com>
2021-06-03 18:15:44 +0200

    edit{Orchestrator/Sources/Gotham_outputs} : lint Gotham_outputs

--- Orchestrator/Sources/Gotham_outputs/syslog_output.py
+++ Orchestrator/Sources/Gotham_outputs/syslog_output.py
@@ -1,18 +1,18 @@
 import subprocess
 import hashlib
 
-#===Logging components===#
+# Logging components
 import os
 import logging
 GOTHAM_HOME = os.environ.get('GOTHAM_HOME')
 logging.basicConfig(filename=GOTHAM_HOME + 'Orchestrator/Logs/gotham.log',
                     level=logging.DEBUG, format='%(asctime)s -- %(name)s -- %(levelname)s -- %(message)s')
-#=======================#
+
 
 def create(new_syslog_output, hostname, syslog_port, protocol):
     '''
     Create a syslog output
-    
+
     ARGUMENTS:
         new_syslog_output (string) : name of the new configuration file
         hostname (string) : hostname of the syslog server
@@ -22,20 +22,25 @@ def create(new_syslog_output, hostname, syslog_port, protocol):
     # Generate rsyslog configuration
     try:
         # Create the configuration file with the right name
-        rsyslog_conf_file = open(f"/etc/rsyslog.d/{str(new_syslog_output)}", "a")
+        rsyslog_conf_file = open(
+            f"/etc/rsyslog.d/{str(new_syslog_output)}",
+            "a"
+        )
         # Monitor the log file of the honeypot
         rsyslog_conf_file.write('if $msg contains "hp-" then {\n')
-        # Apply parsing rules
-        rsyslog_conf_file.write(f'    action(Type="omfwd" Target="{str(hostname)}" Port="{str(syslog_port)}" Protocol="{str(protocol)}" Template="RawFormat")\n')
+        # Apply parsing rules
+        rsyslog_conf_file.write(
+            f'    action(Type="omfwd" Target="{str(hostname)}" Port="{str(syslog_port)}" Protocol="{str(protocol)}" Template="RawFormat")\n')
         # Close the if condition
         rsyslog_conf_file.write('}\n')
         # Monitor the log file of the link
         rsyslog_conf_file.write('if $syslogtag contains "lk-" then {\n')
-        # Apply parsing rules
-        rsyslog_conf_file.write(f'    action(Type="omfwd" Target="{str(hostname)}" Port="{str(syslog_port)}" Protocol="{str(protocol)}" Template="RawFormat")\n')
+        # Apply parsing rules
+        rsyslog_conf_file.write(
+            f'    action(Type="omfwd" Target="{str(hostname)}" Port="{str(syslog_port)}" Protocol="{str(protocol)}" Template="RawFormat")\n')
         # Close the if condition
         rsyslog_conf_file.write('}\n')
-    except Exception as e:
+    except Exception:
         error = "Fail to generate syslog output configuration file"
         logging.error(error)
         raise ValueError(error)
@@ -45,6 +50,7 @@ def create(new_syslog_output, hostname, syslog_port, protocol):
         error = f"Fail to deploy syslog output configuration : {str(e)}"
         logging.error(error)
         raise ValueError(error)
+
 
 def delete(obsolete_syslog_output):
     '''
@@ -69,7 +75,11 @@ def naming(name, hostname, port, protocol):
     '''
     # Hash the parameters
     hash_obj = hashlib.sha1()
-    hash_obj.update(str(name).encode() + str(hostname).encode() + str(port).encode() + str(protocol).encode())
+    hash_obj.update(
+        str(name).encode() +
+        str(hostname).encode() +
+        str(port).encode() +
+        str(protocol).encode())
     hash_name = hash_obj.hexdigest()
     # return the name containing the hash of parameters
     return(f"10-syslog_{str(hash_name)}.conf")
