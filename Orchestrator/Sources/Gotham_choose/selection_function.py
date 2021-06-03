e47c856ce569d0afa6d31d102cad157a72e8916d
spit22 <spit22@protonmail.com>
2021-06-03 12:09:07 +0200

    edit{Orchestrator/Sources/Gotham_choose} : lint Gotham_choose

--- Orchestrator/Sources/Gotham_choose/selection_function.py
+++ Orchestrator/Sources/Gotham_choose/selection_function.py
@@ -1,4 +1,3 @@
-# Import libraries
 import configparser
 
 # Logging components
@@ -10,13 +9,15 @@ logging.basicConfig(filename=GOTHAM_HOME + 'Orchestrator/Logs/gotham.log',
 
 
 def weighting_nb_link(object_type, objects_infos):
-    # Add a weight corresponding to the number of links associated with each object 
-    #
-    #
-    # object_type (string) : type of the object ("hp" or "serv")
-    # objects_infos (list of dict) : list of potentials objects
-    # 
-    # Return list of dict of object with additional weight based on the number of link using the object
+    '''
+    Add a weight corresponding to the number of links associated with each object
+
+    ARGUMENTS:
+        object_type (string) : type of the object ("hp" or "serv")
+        objects_infos (list of dict) : list of potentials objects
+
+    Return list of dict of object with additional weight based on the number of link using the object
+    '''
 
     # Check object_type
     if (object_type != "hp" and object_type != "serv"):
@@ -29,22 +30,33 @@ def weighting_nb_link(object_type, objects_infos):
     config = configparser.ConfigParser()
     config.read(GOTHAM_HOME + 'Orchestrator/Config/config.ini')
     # Retrieve the corresponding weight
-    weight = int(config[object_type+'_weight']["nb_link"])
+    weight = int(config[object_type + '_weight']["nb_link"])
 
-    # Apply weight for each object according to its number of links 
-    objects_infos = [{**object_infos, **{"weight": int(object_infos["weight"])+(weight*int(len(object_infos["link_id"].split("||||||"))))}} if (
-        object_infos["link_id"] != '' and not(object_infos["link_id"] is None) and object_infos["link_id"] != 'NULL') else object_infos for object_infos in objects_infos]
+    # Apply weight for each object according to its number of links
+    objects_infos = [{
+        **object_infos,
+        **{"weight": int(object_infos["weight"]) +
+           (weight * int(len(object_infos["link_id"].split("||||||"))))
+           }
+    } if (
+        object_infos["link_id"] != '' and not(object_infos["link_id"] is None)
+        and object_infos["link_id"] != 'NULL'
+    ) else object_infos for object_infos in objects_infos
+    ]
 
     return objects_infos
 
 
 def weighting_nb_port(servs_infos):
-    # Add a weight corresponding to the number of port used by each server 
-    #
-    #
-    # servs_infos (list of dict) : list of potentials server
-    # 
-    # Return list of dict of serv with additional weight based on the number of port used
+    '''
+    Add a weight corresponding to the number of port used by each server
+
+    ARGUMENTS:
+        servs_infos (list of dict) : list of potentials server
+
+    Return list of dict of serv with additional weight based on the number
+    of port used
+    '''
 
     GOTHAM_HOME = os.environ.get('GOTHAM_HOME')
     # Retrieve settings from config file
@@ -53,21 +65,30 @@ def weighting_nb_port(servs_infos):
     # Retrieve the corresponding weight
     weight = int(config['serv_weight']["nb_port_used"])
 
-    # Apply weight for each object according to its number of port used 
-    servs_infos = [{**serv_infos, **{"weight": int(serv_infos["weight"])+(weight*int(len(serv_infos["lhs_port"].replace("||||||", "||||").split("||||"))))}} if (
-        serv_infos["lhs_port"] != '' and not(serv_infos["lhs_port"] is None) and serv_infos["lhs_port"] != 'NULL') else serv_infos for serv_infos in servs_infos]
+    # Apply weight for each object according to its number of port used
+    servs_infos = [{
+        **serv_infos,
+        **{"weight": int(serv_infos["weight"]) +
+        (weight * int(len(serv_infos["lhs_port"].replace("||||||", "||||").split("||||"))))}
+    } if (
+        serv_infos["lhs_port"] != '' and not(serv_infos["lhs_port"] is None)
+        and serv_infos["lhs_port"] != 'NULL'
+    ) else serv_infos for serv_infos in servs_infos
+    ]
 
     return servs_infos
 
 
 def weighting_state(object_type, objects_infos):
-    # Add a weight corresponding to the state associated with each object 
-    #
-    #
-    # object_type (string) : type of the object ("hp" or "serv")
-    # objects_infos (list of dict) : list of potentials objects
-    # 
-    # Return list of dict of object with additional weight based on the state
+    '''
+    Add a weight corresponding to the state associated with each object
+
+    ARGUMENTS:
+        object_type (string) : type of the object ("hp" or "serv")
+        objects_infos (list of dict) : list of potentials objects
+
+    Return list of dict of object with additional weight based on the state
+    '''
 
     # Check object_type
     if (object_type != "hp" and object_type != "serv"):
@@ -80,25 +101,37 @@ def weighting_state(object_type, objects_infos):
     config = configparser.ConfigParser()
     config.read(GOTHAM_HOME + 'Orchestrator/Config/config.ini')
     # Retrieve the corresponding weight
-    weights = dict(config.items(object_type+'_weight'))
-    
-    # Apply weight for each object according to its state 
-    objects_infos = [{**object_infos, **{"weight": int(object_infos["weight"])+int(weights[object_infos[object_type+"_state"].lower()])}} if object_infos[object_type+"_state"].lower(
-    ) in weights else {**object_infos, **{"weight": int(object_infos["weight"])+int(max([int(i) for i in weights.values()]))}} for object_infos in objects_infos]
+    weights = dict(config.items(object_type + '_weight'))
+
+    # Apply weight for each object according to its state
+    objects_infos = [{
+        **object_infos,
+        **{"weight": int(object_infos["weight"]) + int(weights[object_infos[object_type + "_state"].lower()])}
+    } if (
+        object_infos[object_type + "_state"].lower() in weights
+    ) else {
+        **object_infos,
+        **{"weight": int(object_infos["weight"]) +
+        int(max([int(i) for i in weights.values()]))}
+    } for object_infos in objects_infos
+    ]
 
     return objects_infos
 
 
 def weighting_nb_useless_tags(object_type, objects_infos, tags):
-    # Add a weight corresponding to the number of useless tags associated with each object
-    #
-    #
-    # object_type (string) : type of the object ("hp" or "serv")
-    # objects_infos (list of dict) : list of potentials objects
-    # tags (string) : list of desired tags separate by tag separator
-    # 
-    # Return list of dict of object with additional weight based on the useless tag number
+    '''
+    Add a weight corresponding to the number of useless tags associated
+    with each object
 
+    ARGUMENTS:
+        object_type (string) : type of the object ("hp" or "serv")
+        objects_infos (list of dict) : list of potentials objects
+        tags (string) : list of desired tags separate by tag separator
+
+    Return list of dict of object with additional weight based on the
+    useless tag number
+    '''
 
     # Check object_type
     if (object_type != "hp" and object_type != "serv"):
@@ -111,7 +144,7 @@ def weighting_nb_useless_tags(object_type, objects_infos, tags):
     config = configparser.ConfigParser()
     config.read(GOTHAM_HOME + 'Orchestrator/Config/config.ini')
     # Retrieve the corresponding weight
-    weight = int(config[object_type+'_weight']["nb_useless_tag"])
+    weight = int(config[object_type + '_weight']["nb_useless_tag"])
     # Retrieve the port separator
     separator = config['tag']['separator']
 
@@ -119,20 +152,32 @@ def weighting_nb_useless_tags(object_type, objects_infos, tags):
     tags_list = tags.lower().split(separator)
 
     # Apply weight for each object according to its number of useless tags
-    objects_infos = [{**object_infos, **{"weight": int(object_infos["weight"])+(weight*int(int(len(object_infos[object_type+"_tags"].lower().split(
-        '||')))-int(len(set(object_infos[object_type+"_tags"].lower().split('||')).intersection(tags_list)))))}} for object_infos in objects_infos]
+    objects_infos = [{
+        **object_infos,
+        **{"weight": int(object_infos["weight"]) +
+           (weight * int(int(len(object_infos[object_type +
+            "_tags"].lower().split('||'))) -
+            int(len(set(object_infos[object_type +
+                                     "_tags"
+                                     ].lower().split('||')).intersection(tags_list))))
+            )
+           }
+    } for object_infos in objects_infos]
 
     return objects_infos
 
 
 def weighting_nb_free_port(servs_infos):
-    # Add a weight corresponding to the number of free port associated with each server 
-    #
-    #
-    # servs_infos (list of dict) : list of potentials servs
-    # 
-    # Return list of dict of server with additional weight based on the free port number
+    '''
+    Add a weight corresponding to the number of free port associated with 
+    each server
 
+    ARGUMENTS:
+        servs_infos (list of dict) : list of potentials servs
+
+    Return list of dict of server with additional weight based on the free
+    port number
+    '''
 
     GOTHAM_HOME = os.environ.get('GOTHAM_HOME')
     # Retrieve settings from config file
@@ -143,21 +188,28 @@ def weighting_nb_free_port(servs_infos):
     # Retrieve the port separator
     separator = config['port']['separator']
 
-    # Apply weight for each server according to its number of free port for the link 
-    servs_infos = [{**serv_infos, **{"weight": int(serv_infos["weight"])+(weight*int(
-        len(serv_infos["free_ports"].split(separator))))}} for serv_infos in servs_infos]
+    # Apply weight for each server according to its number of free port for
+    # the link
+    servs_infos = [{
+        **serv_infos,
+        **{"weight": int(serv_infos["weight"]) +
+           (weight * int(len(serv_infos["free_ports"].split(separator))))
+           }
+    } for serv_infos in servs_infos]
 
     return servs_infos
 
 
 def weighting_duplicat(hps_infos):
-    # Add a weight if the honeypot is a duplicat 
-    #
-    #
-    # hps_infos (list of dict) : list of potentials honeypots
-    # 
-    # Return list of dict of honeypot with additional weight if the honeypot is a duplicat
+    '''
+    Add a weight if the honeypot is a duplicat
 
+    ARGUMENTS:
+        hps_infos (list of dict) : list of potentials honeypots
+
+    Return list of dict of honeypot with additional weight if the honeypot
+    is a duplicat
+    '''
 
     GOTHAM_HOME = os.environ.get('GOTHAM_HOME')
     # Retrieve settings from config file
@@ -165,23 +217,33 @@ def weighting_duplicat(hps_infos):
     config.read(GOTHAM_HOME + 'Orchestrator/Config/config.ini')
     # Retrieve the corresponding weight
     weight = int(config['hp_weight']["duplicat"])
-    
 
-    # Apply weight if the hp is a duplicat 
-    hps_infos = [{**hp_infos, **{"weight": (int(hp_infos["weight"])+int(weight) if int(hp_infos["hp_duplicat"])!=0 else int(hp_infos["weight"]))}} for hp_infos in hps_infos]
+    # Apply weight if the hp is a duplicat
+    hps_infos = [{
+        **hp_infos,
+        **{"weight": (int(hp_infos["weight"]) +
+           int(weight) if (
+               int(hp_infos["hp_duplicat"]) != 0
+               ) else int(hp_infos["weight"]))
+           }
+    } for hp_infos in hps_infos]
 
     return hps_infos
 
 
 def weighting_time(object_type, objects_infos, column):
-    # Add a weight corresponding to the creation and modification timestamp associated with each object 
-    #
-    #
-    # object_type (string) : type of the object ("hp" or "serv")
-    # objects_infos (list of dict) : list of potentials objects
-    # column (string) : type of the timestamp ("created_at" or "updated_at")
-    # 
-    # Return list of dict of object with additional weight based on the creation and modification timestamp
+    '''
+    Add a weight corresponding to the creation and modification timestamp
+    associated with each object
+
+    ARGUMENTS:
+        object_type (string) : type of the object ("hp" or "serv")
+        objects_infos (list of dict) : list of potentials objects
+        column (string) : type of the timestamp ("created_at" or "updated_at")
+
+    Return list of dict of object with additional weight based on the
+    creation and modification timestamp
+    '''
 
     # Check object_type
     if (object_type != "hp" and object_type != "serv"):
@@ -200,19 +262,42 @@ def weighting_time(object_type, objects_infos, column):
     config = configparser.ConfigParser()
     config.read(GOTHAM_HOME + 'Orchestrator/Config/config.ini')
     # Retrieve the corresponding weight
-    weight = int(config[object_type+'_weight'][column])
+    weight = int(config[object_type + '_weight'][column])
 
     # List all timestamp of the objects according to the column
-    timestamp_list = [float(object_infos[object_type+"_"+column].timestamp()) for object_infos in objects_infos if (object_infos[object_type +
-                                                                                                                                 '_'+column] != '' and not(object_infos[object_type+'_'+column] is None) and object_infos[object_type+'_'+column] != 'NULL')]
-    # Get the max 
+    timestamp_list = [float(
+        object_infos[object_type + "_" + column].timestamp()
+    ) for object_infos in objects_infos if (
+        object_infos[object_type + '_' + column] != ''
+        and not(object_infos[object_type + '_' + column] is None)
+        and object_infos[object_type + '_' + column] != 'NULL'
+        )
+    ]
+    # Get the max
     max_time = max(timestamp_list)
-    # Get the min 
+    # Get the min
     min_time = min(timestamp_list)
 
-    # Apply weight for each object according to its creation or modification timestamp 
+    # Apply weight for each object according to its creation or modification
+    # timestamp
     if max_time > min_time:
-        objects_infos = [{**object_infos, **{"weight": int(object_infos["weight"])+round(float(weight)*(float(object_infos[object_type+'_'+column].timestamp())-min_time)/(max_time-min_time))}} if (object_infos[object_type+'_'+column] != '' and not(
-            object_infos[object_type+'_'+column] is None) and object_infos[object_type+'_'+column] != 'NULL') else {**object_infos, **{"weight": int(object_infos["weight"])+round(float(weight)/2)}} for object_infos in objects_infos]
+        objects_infos = [{
+            **object_infos,
+            **{"weight": int(object_infos["weight"]) +
+                round(float(weight) * (
+                        float(object_infos[object_type + '_' + column].timestamp())
+                        - min_time
+                       ) / (max_time - min_time)
+                      )
+               }
+        } if (object_infos[object_type + '_' + column] != ''
+              and not(object_infos[object_type + '_' + column] is None)
+              and object_infos[object_type + '_' + column] != 'NULL'
+              ) else {
+                  **object_infos,
+                  **{"weight": int(object_infos["weight"]) +
+                     round(float(weight) / 2)}
+                  } for object_infos in objects_infos
+        ]
 
     return objects_infos
