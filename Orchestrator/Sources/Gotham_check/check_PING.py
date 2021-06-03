ba32737494c8fdc83217ed8b88a53509edc82876
spit22 <spit22@protonmail.com>
2021-06-03 17:51:25 +0200

    edit{Orchestrator/Sources/Gotham_check} : lint gotham_check

--- Orchestrator/Sources/Gotham_check/check_PING.py
+++ Orchestrator/Sources/Gotham_check/check_PING.py
@@ -2,14 +2,20 @@ import subprocess
 
 
 def main(hostname):
-	# Check if a host is alive on the network
-	#
-	# hostname (string) : ip or hostname of the checked server
-	#
-	# Return True if server is alive, False in the other case
+    '''
+    Check if a host is alive on the network
+
+    ARGUMENTS:
+        hostname (string) : ip or hostname of the checked server
+
+    Return True if server is alive, False in the other case
+    '''
     try:
-        subprocess.check_call(['ping', '-c 1', hostname],
-                              stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
+        subprocess.check_call(
+            ['ping', '-c 1', hostname],
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.STDOUT
+        )
         return True
-    except:
+    except BaseException:
         return False
