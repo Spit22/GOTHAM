977843478473317dd8323c83f7b59e00185bb6cf
spit22 <spit22@protonmail.com>
2021-06-03 11:41:42 +0200

    edit{Orchestrator/Sources/Gotham_SSH_SCP} : lint Gotham_SSH_SCP

--- Orchestrator/Sources/Gotham_SSH_SCP/SSH_SCP_classes.py
+++ Orchestrator/Sources/Gotham_SSH_SCP/SSH_SCP_classes.py
@@ -1,16 +1,13 @@
-#===Import external libs===#
 import paramiko
 import scp
 from io import StringIO
 import os
 import logging
-#==========================#
 
-#===Logging components===#
+# Logging components
 GOTHAM_HOME = os.environ.get('GOTHAM_HOME')
 logging.basicConfig(filename=GOTHAM_HOME + 'Orchestrator/Logs/gotham.log',
                     level=logging.DEBUG, format='%(asctime)s -- %(name)s -- %(levelname)s -- %(message)s')
-#=======================#
 
 
 class GothamServer:
@@ -31,7 +28,8 @@ class GothamServer:
         ARGUMENTS:
             hostname (string) : hostname of the remote server
             port (string) : SSH port of the remote host
-            ssh_key (string) : SSH key that allow the orchestrator to connect to the remote host
+            ssh_key (string) : SSH key that allow the orchestrator to connect
+                to the remote host
         '''
         # SSH credentials (username@hostname)
         self.hostname = hostname
@@ -56,7 +54,8 @@ class GothamServer:
             # Check in known_host file
             self.ssh_session.load_system_host_keys()
             self.ssh_session.set_missing_host_key_policy(
-                paramiko.AutoAddPolicy())
+                paramiko.AutoAddPolicy()
+            )
             # Start the SSH session
             self.ssh_session.connect(
                 hostname=self.hostname,
@@ -66,7 +65,9 @@ class GothamServer:
             )
             # Init an SCP session using the SSH session just created
             self.scp_session = scp.SCPClient(self.ssh_session.get_transport())
-        logging.info(f"[+] SSH connection with the remote server {self.hostname} has just started")
+        logging.info(
+            f"[+] SSH connection with the remote server {self.hostname} has just started"
+        )
         return self.ssh_session
 
     def disconnect(self):
@@ -76,7 +77,9 @@ class GothamServer:
         if self.ssh_session:
             try:
                 self.ssh_session.close()
-                logging.info(f"[-] SSH connection with the remote server {self.hostname} has just been closed")
+                logging.info(
+                    f"[-] SSH connection with the remote server {self.hostname} has just been closed"
+                )
             except Exception as e:
                 error = f"SSH disconnection with {self.hostname} failed : {e}"
                 logging.error(error)
@@ -95,7 +98,8 @@ class GothamServer:
 
         ARGUMENTS:
             file_path (list) : local path(s) of the file(s) we want to send
-            remote_file_path (string) : path on the remote host we want to put the file(s)
+            remote_file_path (string) : path on the remote host we want to
+                put the file(s)
         '''
         # Start an SCP connection
         try:
@@ -112,14 +116,16 @@ class GothamServer:
                 remote_path=remote_file_path
             )
             logging.info(
-                f"The file {file_paths} has just been uploaded on the server {self.hostname} in the directory {remote_file_path}")
+                f"The file {file_paths} has just been uploaded on the server {self.hostname} in the directory {remote_file_path}"
+            )
 
     def commands_execution(self, commands):
         '''
         Execute multiple commands
 
         ARGUMENTS:
-            commands (list) : list of the commands we want to execute on the remote server
+            commands (list) : list of the commands we want to execute on the
+                remote server
         '''
         # Start an SSH connection
         try:
@@ -128,13 +134,11 @@ class GothamServer:
             error = f"Connection with {self.hostname} failed : {e}"
             logging.error(error)
             raise ValueError(error)
-
         # Execute all of the commands
         for cmd in commands:
             stdin, stdout, stderr = self.ssh_session.exec_command(cmd)
             for line in stdout.read().splitlines():
-                print("Command return : "+str(line))
-
+                print("Command return : " + str(line))
         logging.info(
             f"The commands {commands} has just been executed on the server {self.hostname}")
 
@@ -152,15 +156,13 @@ class GothamServer:
             error = f"Connection with {self.hostname} failed : {e}"
             logging.error(error)
             raise ValueError(error)
-
         # Execute all of the commands
         stdin, stdout, stderr = self.ssh_session.exec_command(command)
-        answer=[]
+        answer = []
         for line in stdout.read().splitlines():
             if str(line)[0:2] == "b'" or str(line)[0:2] == 'b"':
-                line=str(line)[2:-1]
+                line = str(line)[2:-1]
             answer.append(str(line))
-
         logging.info(
             f"The command {command} has just been executed on the server {self.hostname}")
         return answer
--- Orchestrator/Sources/Gotham_SSH_SCP/__init__.py
+++ Orchestrator/Sources/Gotham_SSH_SCP/__init__.py
@@ -1,6 +1,4 @@
-#===Import GOTHAM's libs===#
 from . import SSH_SCP_classes
-#==========================#
 
 
 def send_file(hostname, port, ssh_key, file_path, remote_file_path):
@@ -10,9 +8,11 @@ def send_file(hostname, port, ssh_key, file_path, remote_file_path):
     ARGUMENTS:
         hostname (string) : hostname of the remote host
         port (string) : SSH port of the remote host
-        ssh_key (string) : SSH key that allow the orchestrator to connect to the remote host
+        ssh_key (string) : SSH key that allow the orchestrator to connect
+            to the remote host
         file_path (list) : list of local path(s) of the file(s) we want to send
-        remote_file_path (string) : path on the remote host we want to put the file(s)
+        remote_file_path (string) : path on the remote host we want to put the
+            file(s)
     '''
     # Init remote_server object
     remote_server = SSH_SCP_classes.GothamServer(hostname, port, ssh_key)
@@ -31,12 +31,14 @@ def send_file(hostname, port, ssh_key, file_path, remote_file_path):
 def execute_commands(hostname, port, ssh_key, commands):
     '''
     Execute commands on a remote server
-    
+
     ARGUMENTS:
         hostname (string) : hostname of the remote host
         port (string) : SSH port of the remote host
-        ssh_key (string) : SSH key that allow the orchestrator to connect to the remote host
-        commands (list) : list of the commands we want to execute on the remote server
+        ssh_key (string) : SSH key that allow the orchestrator to connect
+            to the remote host
+        commands (list) : list of the commands we want to execute on the
+            remote server
     '''
     # Init remote_server object
     remote_server = SSH_SCP_classes.GothamServer(hostname, port, ssh_key)
@@ -55,18 +57,19 @@ def execute_commands(hostname, port, ssh_key, commands):
 def execute_command_with_return(hostname, port, ssh_key, command):
     '''
     Execute command on a remote server
-    
+
     ARGUMENTS:
         hostname (string) : hostname of the remote host
         port (string) : SSH port of the remote host
-        ssh_key (string) : SSH key that allow the orchestrator to connect to the remote host
+        ssh_key (string) : SSH key that allow the orchestrator to connect to
+            the remote host
         command (string) : the command we want to execute on the remote server
     '''
     # Init remote_server object
     remote_server = SSH_SCP_classes.GothamServer(hostname, port, ssh_key)
     # Execute the commands
     try:
-        answer=remote_server.command_execution_with_return(command)
+        answer = remote_server.command_execution_with_return(command)
     except Exception as e:
         raise ValueError(e)
     # Close the connection
@@ -77,17 +80,21 @@ def execute_command_with_return(hostname, port, ssh_key, command):
     return answer
 
 
-def send_file_and_execute_commands(hostname, port, ssh_key, file_path, remote_file_path, commands):
+def send_file_and_execute_commands(
+        hostname, port, ssh_key, file_path, remote_file_path, commands):
     '''
     Send a file to a remote host then execute commands
 
     ARGUMENTS:
         hostname (string) : hostname of the remote host
         port (string) : SSH port of the remote host
-        ssh_key (string) : SSH key that allow the orchestrator to connect to the remote host
+        ssh_key (string) : SSH key that allow the orchestrator to connect
+            to the remote host
         file_path (list) : local path(s) of the file(s) we want to send
-        remote_file_path (string) : path on the remote host we want to put the file(s)
-        commands (list) : list of the commands we want to execute on the remote server
+        remote_file_path (string) : path on the remote host we want to put
+            the file(s)
+        commands (list) : list of the commands we want to execute on the
+            remote server
     '''
     # Init remote_server object
     remote_server = SSH_SCP_classes.GothamServer(hostname, port, ssh_key)
