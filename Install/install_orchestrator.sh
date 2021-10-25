92a5c05d9b3071bc187b9bc48b2313dba0cac44a
Iole-Bri <eloibriere@gmail.com>
2021-10-25 11:13:05 +0200

    edit{Install/docker_gotham_orchestrator}: packages

--- Install/install_orchestrator.sh
+++ Install/install_orchestrator.sh
@@ -55,7 +55,7 @@ $APT update > /dev/null 2>&1
 echo "[+] APT updated"
 
 # Install needed packages
-$APT install -y python3 python3-pip git mariadb-server git rsyslog > /dev/null 2>&1
+$APT install -y python3 python3-pip mariadb-server > /dev/null 2>&1
 PIP3=$(which pip3)
 GIT=$(which git)
 echo "[+] Some packages correctly installed"
