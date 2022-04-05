91673764ca3d7f2529502d10c97c324bf73b2020
Spit22 <spit22@protonmail.com>
2022-04-06 00:42:17 +0200

    edit{Install/install_orchestrator} : improve script

--- Install/install_orchestrator.sh
+++ Install/install_orchestrator.sh
@@ -3,9 +3,15 @@
 # Authors : GOTHAM Team
 # Descr : this script permits to prepare te orchestrator system and install GOTHAM
 #
-# Last update : 31/05/2021
+# Last update : 06/04/2022
 
-set -e pipefail
+set -euo pipefail
+
+### COLOR CODES ###
+red="\e[0;91m"
+blue="\e[0;94m"
+green="\e[0;92m"
+default="\e[0m"
 
 ### DEFINE GLOBAL VARIABLES ###
 GOTHAM_HOME=/opt/GOTHAM/
@@ -13,29 +19,29 @@ GOTHAM_GIT="https://github.com/Spit22/GOTHAM"
 EXEC_BRANCH="remotes/origin/dev_V1"
 
 ### DEFINE BINARY PATHS ###
-USERADD=$(which useradd)
-CHOWN=$(which chown)
-CHMOD=$(which chmod)
-APT=$(which apt)
-RM=$(which rm)
+USERADD=$(which useradd) || echo "${red}[-] Unknown command : useradd${default}" && exit 1
+CHOWN=$(which chown) || echo "${red}[-] Unknown command : chown${default}" && exit 1
+CHMOD=$(which chmod) || echo "${red}[-] Unknown command : chmod${default}" && exit 1
+APT=$(which apt) || echo "${red}[-] Unknown command : apt${default}" && exit 1
+RM=$(which rm) || echo "${red}[-] Unknown command : rm${default}" && exit 1
 
 ### CHECK IF ROOT ###
 if [ "$EUID" -ne 0 ]
-  then echo "[-] Please run this script as root"
-  exit
+  then echo "${red}[-] Please run this script as root${default}"
+  exit 1
 fi
 
 ### PREPARE GOTHAM SYSTEM ###
-echo "=== Preparing the gotham system... ==="
+echo "${blue}=== Preparing the gotham system... ===${default}"
 
 # Create GOTHAM user as an alias for the root account
 $USERADD -o -u 0 -g 0 -N -d /root/ -M gotham > /dev/null 2>&1
-echo "[+] User gotham created"
+echo "${green}[+] User gotham created${default}"
 
 # Generate the GOTHAM profile
 echo -e "#GOTHAM global variables\nexport GOTHAM_HOME=$GOTHAM_HOME" > /etc/profile.d/gotham.sh
 $CHMOD +x /etc/profile.d/gotham.sh
-echo "[+] Gotham profile generated under /etc/profile.d/gotham.sh"
+echo "${green}[+] Gotham profile generated under /etc/profile.d/gotham.sh${default}"
 
 # Create some paths used by GOTHAM
 mkdir -p /data/template
@@ -45,28 +51,28 @@ mkdir -p /data/honeypot-log
 mkdir -p /data/link-log
 mkdir -p /data/rsyslog/rulebase
 mkdir -p /etc/rsyslog.d
-echo "[+] All paths created"
+echo "${green}[+] All paths created${default}"
 
 
 ### INSTALL DEPENDANCIES ###
-echo "=== Installing dependancies... ==="
+echo "${blue}=== Installing dependancies... ===${default}"
 
 # Update repository
 $APT update > /dev/null 2>&1
-echo "[+] APT updated"
+echo "${green}[+] APT updated${default}"
 
 # Install needed packages
 $APT install -y python3 python3-pip git mariadb-server git rsyslog > /dev/null 2>&1
 PIP3=$(which pip3)
 GIT=$(which git)
-echo "[+] Some packages correctly installed"
+echo "${green}[+] Some packages correctly installed${default}"
 
 # Install libs
 $APT install -y libmariadb-dev > /dev/null 2>&1
-echo "[+] Some libraries correctly installed"
+echo "${green}[+] Some libraries correctly installed${default}"
 
 ### INSTALL RSYSLOG ###
-echo "=== Configuring Rsyslog... ==="
+echo "${blue}=== Configuring Rsyslog... ===${default}"
 
 # Pre-configure rsyslog
 touch /etc/rsyslog.d/00-syslog_server.conf
@@ -78,27 +84,27 @@ input(type="imtcp" port="1514")
 
 # Restart rsyslog
 systemctl restart rsyslog > /dev/null 2>&1
-echo "[+] Rsyslog configured under /etc/rsyslog.d/"
+echo "${green}[+] Rsyslog configured under /etc/rsyslog.d/${default}"
 
 
 ### INSTALL GOTHAM ###
-echo "=== Installing GOTHAM... ==="
+echo "${blue}=== Installing GOTHAM... ===${default}"
 
 # Clone repository on this directory
 $GIT clone $GOTHAM_GIT $GOTHAM_HOME > /dev/null 2>&1
 $CHOWN -R gotham:root $GOTHAM_HOME
-echo "[+] GOTHAM cloned under $GOTHAM_HOME"
+echo "${green}[+] GOTHAM cloned under $GOTHAM_HOME${default}"
 
 # Going on the repository folder
 cd $GOTHAM_HOME
 
 # Choose the git branch
 $GIT checkout $EXEC_BRANCH > /dev/null 2>&1
-echo "[+] Switched to $EXEC_BRANCH"
+echo "${green}[+] Switched to $EXEC_BRANCH${default}"
 
 # Install all python libs
 $PIP3 install -r $GOTHAM_HOME/Orchestrator/Sources/requirements.txt > /dev/null 2>&1
-echo "[+] Pip dependancies installed"
+echo "${green}[+] Pip dependancies installed${default}"
 
 # Create the log folder
 mkdir -p $GOTHAM_HOME/Orchestrator/Logs
@@ -106,4 +112,4 @@ mkdir -p $GOTHAM_HOME/Orchestrator/Logs
 # Configure database
 mysql -u root -e "CREATE DATABASE GOTHAM;" > /dev/null 2>&1
 mysql -u root GOTHAM < $GOTHAM_HOME/Orchestrator/Internal_Database/gotham.sql > /dev/null 2>&1
-echo "[+] MySQL db successfully imported"
+echo "${green}[+] MySQL db successfully imported${default}"
