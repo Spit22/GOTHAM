ccaf6107478677d982b3e020ec4480ed8bb14b87
Spit22 <spit22@protonmail.com>
2022-04-07 15:11:23 +0200

    edit{Install} : improve install scripts

--- Install/install_dc.sh
+++ Install/install_dc.sh
@@ -3,53 +3,70 @@
 # Authors : GOTHAM Team
 # Descr : this script permits to prepare te orchestrator system and install GOTHAM
 #
-# Last update : 31/05/2021
-set -e pipefail
+# Last update : 07/04/2022
+
+# Production mode
+set -euo pipefail
+
+# Debug mode
+#set -euxo pipefail
 
 ### DEFINE GLOBAL VARIABLES ###
 
+### COLOR CODES ###
+red="\e[0;91m"
+blue="\e[0;94m"
+green="\e[0;92m"
+yellow="\e[0;33m"
+default="\e[0m"
+
 ### DEFINE BINARY PATHS ###
-USERADD=$(which useradd)
-USERMOD=$(which usermod)
-CHOWN=$(which chown)
-CHMOD=$(which chmod)
-APT=$(which apt)
-APTKEY=$(which apt-key)
-KEYGEN=$(which ssh-keygen)
-ENCODE=$(which base64)
+USERADD=$(which useradd) || (echo -e "${red}[-] Unknown command : useradd${default}" && exit 1)
+USERMOD=$(which usermod) || (echo -e "${red}[-] Unknown command : usermod${default}" && exit 1)
+CHOWN=$(which chown) || (echo -e "${red}[-] Unknown command : chown${default}" && exit 1)
+CHMOD=$(which chmod) || (echo -e "${red}[-] Unknown command : chmod${default}" && exit 1)
+APT=$(which apt) || (echo -e "${red}[-] Unknown command : apt${default}" && exit 1)
+APTKEY=$(which apt-key) || (echo -e "${red}[-] Unknown command : apt-key${default}" && exit 1)
+KEYGEN=$(which ssh-keygen) || (echo -e "${red}[-] Unknown command : ssh-keygen${default}" && exit 1)
+ENCODE=$(which base64) || (echo -e "${red}[-] Unknown command : base64${default}" && exit 1)
 
 ### CHECK IF ROOT ###
 if [ "$EUID" -ne 0 ]
-  then echo "[-] Please run this script as root"
+  then echo -e "${red}[-] Please run this script as root${default}"
   exit
 fi
 
 ### PREPARE GOTHAM SYSTEM ###
-echo "=== Preparing the gotham system... ==="
+echo -e "${blue}=== Prepare the gotham system ===${default}"
 
 # Create GOTHAM user as an alias for the root account
-$USERADD -o -u 0 -g 0 -N -d /root/ -M gotham > /dev/null 2>&1
+echo -ne "${yellow}[...] Creating gotham user${default}"
+$USERADD -o -u 0 -g 0 -N -d /root/ -M gotham > /dev/null 2>&1  || (echo -ne "${red}\n[-] Error creating gotham user\n${default}" && exit 1)
+echo -ne "${green}\r[+] gotham user created\n${default}"
 
 # Configure password
+echo -ne "${yellow}[...] Configuring gotham's password${default}"
 mdp=$(tr -cd '[:alnum:]' < /dev/urandom | fold -w50 | head -n1)
-$USERMOD -p "$mdp" gotham
-echo "[+] User gotham created with password : $mdp"
+$USERMOD -p "$mdp" gotham || (echo -ne "${red}\n[-] Error configuring gotham's password\n${default}" && exit 1)
+echo -ne "${green}\r[+] User gotham created with password : $mdp\n${default}"
 
 # Create the folder used by GOTHAM
 mkdir -p /data/tmp
 mkdir -p /data/rsyslog/rulebase
 mkdir -p /etc/rsyslog.d
 mkdir -p /etc/rsyslog.d
-echo "[+] All paths created"
+echo -e "${green}[+] All paths created${default}"
 
 ### INSTALL DEPENDANCIES ###
-echo "=== Installing dependancies... ==="
+echo -e "${blue}=== Installing dependancies... ===${default}"
 
 # Update repo
-$APT update > /dev/null 2>&1
-echo "[+] APT updated"
+echo -ne "${yellow}[...] Updating system${default}"
+$APT update > /dev/null 2>&1 || (echo -ne "${red}\n[-] Error updating system\n${default}" && exit 1)
+echo -ne "${green}\r[+] System updated\n${default}"
 
-# Install utils
+# Install packages
+echo -ne "${yellow}[...] Installing packages${default}"
 $APT install -y \
     apt-transport-https \
     ca-certificates \
@@ -57,40 +74,47 @@ $APT install -y \
     git \
     gnupg-agent \
     rsyslog \
-    software-properties-common > /dev/null 2>&1
+    software-properties-common > /dev/null 2>&1 || (echo -ne "${red}\n[-] Error installing packages\n${default}" && exit 1)
 CURL=$(which curl)
 GIT=$(which git)
-echo "[+] Some packages successfully installed"
+echo -ne "${green}\r[+] Packages correctly installed\n${default}"
 
 # Add docker's GPG key
-$CURL -fsSL https://download.docker.com/linux/debian/gpg | apt-key add - > /dev/null 2>&1
-echo "[+] Docker apt key configured"
+echo -ne "${yellow}[...] Installing Docker's GPG key${default}"
+$CURL -fsSL https://download.docker.com/linux/debian/gpg | apt-key add - > /dev/null 2>&1 || (echo -ne "${red}\n[-] Error installing Docker's GPG key\n${default}" && exit 1)
+echo -ne "${green}\r[+] Docker GPG key installed\n${default}"
 
 # Check we have the correct key fingerprint
-$APTKEY fingerprint 0EBFCD88 | grep "docker@docker.com" > /dev/null 2>&1
-if [ $? != 0 ]; then
-	echo "Key is not valid"
-	exit
-fi
+echo -ne "${yellow}[...] Checking Docker's GPG key${default}"
+$APTKEY fingerprint 0EBFCD88 | grep "docker@docker.com" > /dev/null 2>&1 || (echo -ne "${red}\n[-] Docker's key is invalid\n${default}" && exit 1)
+echo -ne "${green}\r[+] Docker GPG key checked\n${default}"
 
 # Add repository
+echo -ne "${yellow}[...] Adding docker apt repository${default}"
 add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/debian \
    $(lsb_release -cs) \
-   stable" > /dev/null 2>&1
+   stable" > /dev/null 2>&1 || (echo -ne "${red}\n[-] Error adding docker apt repository\n${default}" && exit 1)
+echo -ne "${green}\r[+] Docker apt repository added\n${default}"
 
-# Update repo again
-$APT update > /dev/null 2>&1
+# Update repo
+echo -ne "${yellow}[...] Updating system${default}"
+$APT update > /dev/null 2>&1 || (echo -ne "${red}\n[-] Error updating system\n${default}" && exit 1)
+echo -ne "${green}\r[+] System updated       \n${default}"
+
 # Install docker
-$APT install -y docker-ce docker-ce-cli containerd.io docker-compose > /dev/null 2>&1
+echo -ne "${yellow}[...] Installing docker${default}"
+$APT install -y docker-ce docker-ce-cli containerd.io docker-compose > /dev/null 2>&1 || (echo -ne "${red}\n[-] Error installing docker\n${default}" && exit 1)
 DOCKER=$(which docker)
+echo -ne "${green}\r[+] Docker installed         \n${default}"
 
 # Test docker
-$DOCKER run hello-world > /dev/null 2>&1
-echo "[+] Docker successfully installed"
+echo -ne "${yellow}[...] Testing docker installation${default}"
+$DOCKER run hello-world > /dev/null 2>&1 || (echo -ne "${red}\n[-] Docker failed to run\n${default}" && exit 1)
+echo -ne "${green}\r[+] Docker successfully tested         \n${default}"
 
 ### INSTALL RSYSLOG ###
-echo "=== Configuring Rsyslog... ==="
+echo -e "${blue}=== Configure Rsyslog ===${default}"
 
 # Pre-configure rsyslog
 touch /etc/rsyslog.d/00-JSON_template.conf
@@ -117,19 +141,22 @@ template(name="default-template" type="list") {
 ''' > /etc/rsyslog.d/00-JSON_template.conf
 
 # Restart rsyslog
-systemctl restart rsyslog > /dev/null 2>&1
-echo "[+] Rsyslog configured"
+echo -ne "${yellow}[...] Configuring rsyslog${default}"
+systemctl restart rsyslog > /dev/null 2>&1 || (echo -ne "${red}\n[-] Error restarting rsyslog\n${default}" && exit 1)
+echo -ne "${green}\r[+] Rsyslog configured under /etc/rsyslog.d/\n${default}"
 
 ### INSTALL AND CONFIGURE SSH SERVER ###
-echo "=== Installing and configuring SSH... ==="
+echo -e "${blue}=== Install and configure SSH server ===${default}"
 
 # Update repo
-$APT update > /dev/null 2>&1
-echo "[+] APT updated"
+echo -ne "${yellow}[...] Updating system${default}"
+$APT update > /dev/null 2>&1 || (echo -ne "${red}\n[-] Error updating system\n${default}" && exit 1)
+echo -ne "${green}\r[+] System updated       \n${default}"
 
 # Installation of openssh and base64
-$APT install -y openssh-server > /dev/null 2>&1
-echo "[+] SSH Server installed"
+echo -ne "${yellow}[...] Installing OpenSSH${default}"
+$APT install -y openssh-server > /dev/null 2>&1 || (echo -ne "${red}\n[-] Error installing OpenSSH\n${default}" && exit 1)
+echo -ne "${green}\r[+] OpenSSH server installed       \n${default}"
 
 # Harden SSH configuration
 echo """
@@ -143,14 +170,18 @@ PubkeyAuthentication yes
 """ > /etc/ssh/sshd_config
 
 # Restart openssh
-systemctl restart ssh > /dev/null 2>&1
-echo "[+] SSH Server configured"
+echo -ne "${yellow}[...] Restarting ssh server${default}"
+systemctl restart ssh > /dev/null 2>&1 || (echo -ne "${red}\n[-] ssh server failed to restart\n${default}" && exit 1)
+echo -ne "${green}\r[+] ssh server configured       \n${default}"
 
 ### GENERATE SSH KEY ###
-echo "=== Generating SSH Key... ==="
+echo -e "${blue}=== Generate SSH Key ===${default}"
 
 # Generate a rsa 4096 key pair without passphrase
-$KEYGEN -b 4096 -t rsa -f /root/gotham_key -N "" > /dev/null 2>&1
+echo -ne "${yellow}[...] Generating SSH key${default}"
+$KEYGEN -b 4096 -t rsa -f /root/gotham_key -N "" > /dev/null 2>&1 || (echo -ne "${red}\n[-] Error generating ssh key\n${default}" && exit 1)
+echo -ne "${green}\r[+] ssh server configured       \n${default}"
+
 
 # Copy the public key to authorized keys of root users
 mkdir -p /root/.ssh
@@ -159,7 +190,9 @@ echo $pubkey >> /root/.ssh/authorized_keys
 $CHMOD -R 600 /root/.ssh
 
 # Restart again ssh
-/usr/sbin/service ssh restart > /dev/null 2>&1
+echo -ne "${yellow}[...] Restarting ssh server${default}"
+/usr/sbin/service ssh restart > /dev/null 2>&1 || (echo -ne "${red}\n[-] Error restarting ssh server\n${default}" && exit 1)
+echo -ne "${green}\r[+] ssh server restarted       \n${default}"
 
 # Show base64 encoded private key user has to send to api
 echo -e "\n\n[+]Private key : \n"
--- Install/install_orchestrator.sh
+++ Install/install_orchestrator.sh
@@ -3,9 +3,13 @@
 # Authors : GOTHAM Team
 # Descr : this script permits to prepare te orchestrator system and install GOTHAM
 #
-# Last update : 06/04/2022
+# Last update : 07/04/2022
 
+# Production mode
 set -euo pipefail
+
+# Debug mode
+#set -euxo pipefail
 
 ### COLOR CODES ###
 red="\e[0;91m"
@@ -36,13 +40,15 @@ fi
 echo -e "${blue}=== Prepare the gotham system ===${default}"
 
 # Create GOTHAM user as an alias for the root account
-$USERADD -o -u 0 -g 0 -N -d /root/ -M gotham > /dev/null 2>&1
-echo -e "${green}[+] User gotham created${default}"
+echo -ne "${yellow}[...] Creating gotham user${default}"
+$USERADD -o -u 0 -g 0 -N -d /root/ -M gotham > /dev/null 2>&1  || (echo -ne "${red}\n[-] Error creating gotham user\n${default}" && exit 1)
+echo -ne "${green}\r[+] gotham user created\n${default}"
 
 # Generate the GOTHAM profile
+echo -ne "${yellow}[...] Generating GOTHAM profile${default}"
 echo -e "#GOTHAM global variables\nexport GOTHAM_HOME=$GOTHAM_HOME" > /etc/profile.d/gotham.sh
-$CHMOD +x /etc/profile.d/gotham.sh
-echo -e "${green}[+] Gotham profile generated under /etc/profile.d/gotham.sh${default}"
+$CHMOD +x /etc/profile.d/gotham.sh || (echo -ne "${red}\n[-] Error generating gotham profile\n${default}" && exit 1)
+echo -ne "${green}\r[+] Gotham profile generated under /etc/profile.d/gotham.sh\n${default}"
 
 # Create some paths used by GOTHAM
 mkdir -p /data/template
@@ -60,19 +66,19 @@ echo -e "${blue}=== Install dependancies ===${default}"
 
 # Update repository
 echo -ne "${yellow}[...] Updating system${default}"
-$APT update > /dev/null 2>&1
+$APT update > /dev/null 2>&1 || (echo -ne "${red}\n[-] Error updating system\n${default}" && exit 1)
 echo -ne "${green}\r[+] System updated\n${default}"
 
 # Install needed packages
 echo -ne "${yellow}[...] Installing packages${default}"
-$APT install -y python3 python3-pip git mariadb-server git rsyslog > /dev/null 2>&1
+$APT install -y python3 python3-pip git mariadb-server git rsyslog > /dev/null 2>&1 || (echo -ne "${red}\n[-] Error installing packages\n${default}" && exit 1)
 PIP3=$(which pip3)
 GIT=$(which git)
 echo -ne "${green}\r[+] Packages correctly installed\n${default}"
 
 # Install libs
 echo -ne "${yellow}[...] Installing libraries${default}"
-$APT install -y libmariadb-dev > /dev/null 2>&1
+$APT install -y libmariadb-dev > /dev/null 2>&1 || (echo -ne "${red}\n[-] Error installing libraries\n${default}" && exit 1)
 echo -ne "${green}\r[+] Libraries correctly installed\n${default}"
 
 ### INSTALL RSYSLOG ###
@@ -88,7 +94,7 @@ input(type="imtcp" port="1514")
 
 # Restart rsyslog
 echo -ne "${yellow}[...] Configuring rsyslog${default}"
-systemctl restart rsyslog > /dev/null 2>&1
+systemctl restart rsyslog > /dev/null 2>&1 || (echo -ne "${red}\n[-] Error restarting rsyslog\n${default}" && exit 1)
 echo -ne "${green}\r[+] Rsyslog configured under /etc/rsyslog.d/\n${default}"
 
 
@@ -110,7 +116,7 @@ echo -e "${green}[+] Switched to $EXEC_BRANCH${default}"
 
 # Install all python libs
 echo -ne "${yellow}[...] Installing python dependancies${default}"
-$PIP3 install -r $GOTHAM_HOME/Orchestrator/Sources/requirements.txt > /dev/null 2>&1
+$PIP3 install -r $GOTHAM_HOME/Orchestrator/Sources/requirements.txt > /dev/null 2>&1 || (echo -ne "${red}\n[-] Error installing python dependancies\n${default}" && exit 1)
 echo -ne "${green}\r[+] Python dependancies installed\n${default}"
 
 # Create the log folder
